fa96d5cd6a40fd1ded5695d3bf53aec90b5e89c8
Olesya Miller <olesyam.miller@gmail.com>
2019-12-10 23:23:43 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array_of_names)
+  array_of_names.collect { |name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(speakers)
+  speakers.each_with_index.map { |name, index| "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
+end
+
+def printer(speakers)
+  batch_badge_creator(speakers).each { |element| puts element  }
+  assign_rooms(speakers).each { |element| puts element }
+end
+
+
