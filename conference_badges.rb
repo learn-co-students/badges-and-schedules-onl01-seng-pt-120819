736ae1f69f8495f94136afa78eba61e0ad189ac2
S. Makai Takori <makait87@gmail.com>
2019-12-16 20:15:40 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,17 @@
 # Write your code here.
 def badge_maker(name)
-  badge_message = ("Hello, my name is #{name}.") 
+  "Hello, my name is #{name}." 
 end  
 
 def batch_badge_creator(names)
-  names.each{|name| puts name }
+  array = []
+  names.each{|name| array.push(badge_maker(name))}
+  return array 
 end 
+
+def assign_rooms(names,rooms)
+  array = []
+  name = names.each_with_index {|name, i|} 
+  room = rooms.each_with_index {|room, i|} 
+  
+end
