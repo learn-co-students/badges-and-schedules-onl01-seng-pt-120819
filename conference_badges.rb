1c332a77513980ca7120049e7b43dc2ec42eb030
dwane richards <dwanerich@gmail.com>
2019-12-17 01:08:05 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,28 +1,15 @@
 def badge_maker(name)
-"Hello, my name is #{name}."
+  "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees)
-
-  attendees.map do |list| 
-    badge_maker(list)
-  end
+def batch_badge_creator(array)
+    array.map do |tags|
+    badge_maker(tags)
+    end
 end
 
-def assign_rooms(speakers)
-  
-  speakers.map.with_index do |name, index|
+def assign_rooms(speaker)
+    speaker.map.with_index do |name,index|
     "Hello, #{name}! You'll be assigned to room #{index +1}!"
-  end
-end
-
-def printer(attendees)
-  
-  batch_badge_creator(attendees).map do |list|
-    puts list
-  end
-  
-  assign_rooms(attendees).map do |list|
-    puts list
-  end
+    end
 end
