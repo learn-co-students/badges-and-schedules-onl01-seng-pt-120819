299f99cb3f6aeeb642ca2e7d2c775f337a24a0de
Jeffrey Marbury <jeff.marbury@gmail.com>
2019-12-18 19:20:28 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+return badge_maker("Arel")
+
+attendees = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
+
+def batch_badge_creator(attendees)
+  attendees.each do |speaker|
+    badge = badge_maker(speaker)
+    #puts badge
+  end
+end
+
+return batch_badge_creator(attendees)
