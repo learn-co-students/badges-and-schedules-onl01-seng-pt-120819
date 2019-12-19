6b367ec0c729bcb138ffc65722c4cdc5e9fe29b6
Firas Chaabani <firaschaabani1@gmail.com>
2019-12-19 02:00:14 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
 # Write your code here.
+def badge_maker("name")
+  var "name" ==x
+  return "Hello, my name is 'x'"
+end
+rooms =[1,2,3,4,5,6,7]
+def assign_rooms (name)
+  assign_rooms.each ('x')
+end
